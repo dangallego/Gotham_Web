82075eaa1be10a292253ebcbd1432fbead0cf6d4
Daniel Gallego <dangallego3@gmail.com>
2024-06-28 09:33:30 -0400

    adds read_fils workflow created by Janvi. Adds WIP methods to filaments.py for working with filament dictionary -- still WIPI

--- FILAMENTS/filaments/filaments.py
+++ FILAMENTS/filaments/filaments.py
@@ -1,6 +1,13 @@
 import numpy as np 
 import pandas as pd 
-
+import matplotlib.pyplot as plt
+import matplotlib
+from mpl_toolkits import mplot3d
+from matplotlib import cm
+import mpl_toolkits.mplot3d.art3d as art3d
+import os
+import pandas as pd
+import sys
 
 def ploting_fills(fillament_idx, filament_dict,ax,colorfil="teal"):
     """
@@ -43,3 +50,112 @@ def plotting_fills_2D(fillament_idx, filament_dict,L,colorfil="black",offset=0.5
         py.append(py1)
         
     fil = plt.plot(px,py,c=colorfil,lw='2',alpha=0.5) 
+
+
+
+
+#### ROUTINES BELOW TO IMPORT DATA AUTOMATICALLY AND MAKE PLOTS
+    
+def import_filaments(path_to_filament_NDskl):
+    '''
+    Imports filaments and creates dictionary containing filament coordinates and metrics
+    based on a given path to filament skeleton file. 
+
+    Parameters: 
+    path_to_filament_NDskl: Path fo file containing filament skeleton (.NDskl file)
+
+    Returns:
+    filament_dm_dict (dict): Dictionary of filaments containing coordinates and other filament metrics obtained from DisPerSE. 
+    '''
+    # Imports the methods created by Janvi to read filaments 
+    from . import read_fils as rf
+    # Redirect to where filament .NDskl skeleton file from DisPerSE is located 
+    sys.path.insert(0, path_to_filament_NDskl)
+    import filaments as fils
+
+    skeleton_file_dm = path_to_filament_NDskl
+    filaments_dm = rf.ReadFilament(skeleton_file_dm)
+    filament_dm_dict = filaments_dm.filament_dict
+
+    #separate 'filaments' and 'critical_points' dictionaries (each is now a list of dictionaries)
+    fils = filament_dm_dict['filaments'] ; crit_points = filament_dm_dict['critical_points']
+    #number of filaments
+    nfils = filament_dm_dict['nfils'] ; ncrit = filament_dm_dict['ncrit']
+
+    voids = dict_slice(crit_points, 'cp_idx',0)
+    walls = dict_slice(crit_points, 'cp_idx',1)
+    saddles = dict_slice(crit_points, 'cp_idx',2)   #filament saddles
+    peaks = dict_slice(crit_points, 'cp_idx',3)     #peaks -- nodes! 
+    bi_points = dict_slice(crit_points, 'cp_idx',4) #bifurcation points
+
+    return filament_dm_dict
+
+        
+
+#function to slice a dictionary list by a particular set of keys 
+def dict_slice(dict, key, value):
+    '''Indexes/slices a subset of a larger dictionary. 
+        For use with separating critical points (since the CP id's are gven).
+        Output is a list of dictionaries.'''
+    N = len(dict)
+    list = []
+    for i in range(N):
+        C = dict[i][key]
+        if C == value:
+            list.append(dict[i])
+    return list 
+
+
+#function to find saddle critical points with desired number of nfils attached
+def cp_nfils(critical_points, CP_ID, nfils):
+    '''Takes dicitonary of critical points and input of desired nfils.
+        Returns dicitonary of desired critical_point ID type with 
+        the desired number of filaments attached. 
+        Mainly for use with saddles to get saddles with nfils  == 2.'''
+    cp_type = dict_slice(critical_points, 'cp_idx', CP_ID)
+    cp_nfils = [] ; N = len(cp_type) 
+
+    for i in range(N): 
+        nfils = nfils
+        if cp_type[i]['nfil'] == nfils: 
+            cp_nfils.append(cp_type[i])
+    return(cp_nfils)
+
+
+#function that gives the filaments with cp indices corresponding to nfil == 2
+def filaments_nfil2(filament_dict, critical_points):
+    '''Builds on cp_nfils array by taking the result of finding 
+        saddles with nfil == 2 and using the \'filID\'s\' of each
+         of those saddles to slice the original filament dictionary.
+         Result is a reduced dictionary of what should be proper half filaments 
+         (each filament in the dictionary corresponds to saddles with nfils == 2).'''
+    saddles_nfils = cp_nfils(critical_points, 2, 2) #creates selective saddle dictionary with nfil == 2
+    #finds index of half filaments 
+    filIDs = np.zeros(len(saddles_nfils)) ; filIDs2 = np.zeros(len(saddles_nfils))
+    for i, filID in enumerate(saddles_nfils):
+        filIDs[i] = saddles_nfils[i]['destID,filID'][0][1]
+        filIDs2[i] = saddles_nfils[i]['destID,filID'][1][1]
+    fil_ids = np.append(filIDs, filIDs2) ; fil_ids = np.sort(fil_ids) ; fil_ids = (np.rint(fil_ids)).astype(int)
+    fils_nfils2 = [filament_dict[i] for i in fil_ids]
+    return fils_nfils2
+
+
+#function to get the coordinates (if px,py, and pz are separate) for each CP type
+def cp_plotter(cp):
+    '''If the coordinates for a CP are separated by x,y,z 
+        then this returns an array with all the values for 
+        that critical point.(Perhaps too specialized - come back 
+        and generalize for other use besides coordinates)'''
+    N = len(cp)
+    coordinates = np.zeros((N,3))
+    x = 3
+    for i in range(N): 
+        coordinates[i,0] = cp[i]['px']
+        coordinates[i,1] = cp[i]['py']
+        coordinates[i,2] = cp[i]['pz']
+    x = coordinates[:,0] ; y = coordinates[:,1] ; z = coordinates[:,2]
+    return x, y, z
+
+
+
+
---
+++ FILAMENTS/filaments/read_fils.py
@@ -0,0 +1,443 @@
+# read_fil functions: #intialization file and functions/classes created by Janvi Madhani 
+
+import os
+from os.path import join
+import re
+import numpy as np
+import time as time
+from scipy.io import FortranFile
+import ctypes as c
+import struct
+
+
+class ReadFilament2D:
+    def __init__(self,file_path=None):
+        
+        """
+        Make a filaments dictionary out of ASCII NDSKL file
+        """
+        self.file_path = file_path
+        self.filament_dict = None
+        self.read_data()
+   
+
+        
+    def read_data(self):
+        t0 = time.time()
+        self.filament_dict = {}
+
+        #read the file first and write each line into data
+        data = []
+        f = open(self.file_path,'r')
+        for line in f:
+            data.append(line)
+        f.close()
+        
+        def convert_to_list(ascii_chars,type=float):
+            #strip whitespace from ends
+            ascii_chars = str(ascii_chars)
+
+            char_list = list(ascii_chars.split(" "))
+            char_list = ' '.join(char_list).split()
+            
+
+            p_list = list(map(type, char_list))
+            
+            return p_list
+        
+        header1 = data[0]
+        print('header1,',header1)
+        
+        ndims = data[1]
+        print('ndims,', ndims)
+
+        comments = data[2]
+        print('Comments,',comments)
+
+        extent = data[3]
+        print('Bounding box,', extent)
+
+        #data[4] is str(Critical Points)
+
+        ncrit = int(data[5])
+        print('ncrit,', ncrit)
+        self.filament_dict['ncrit'] = ncrit
+
+        #store all data for critical points in here
+        self.filament_dict['critical_points'] = []
+
+        ##### CPs
+        
+        add_to_idx = 6 
+        for i in range(ncrit):
+            cp_dict = {}
+            i = 0
+            
+            i += add_to_idx #make sure you are at the right line in the data list 
+            critical_vals = data[i]
+            
+            c_idx, px, py,  value, pairID, boundary = convert_to_list(critical_vals)
+            #next line in data
+
+            cp_dict['cp_idx']  = c_idx
+            cp_dict['px'] = px 
+            cp_dict['py'] = py
+            cp_dict['pair_ID'] = pairID
+            cp_dict['boundary'] = boundary 
+
+
+            i += 1
+            nfil = int(data[i])
+            cp_dict['nfil'] = nfil
+            cp_dict['destID,filID'] = []
+            
+            for k in range(nfil):
+
+                i += 1
+                cp_on_fil = data[i]
+                destID, filID = convert_to_list(cp_on_fil,int)
+        
+                cp_dict['destID,filID'].append([destID,filID])
+
+            #make this to fill out later
+            cp_dict['Field Vals'] = []
+            #add all info to cp dict
+            self.filament_dict['critical_points'].append(cp_dict)
+            
+            add_to_idx = i + 1
+
+
+        ##### Filaments
+
+        fil_idx = i + 1
+        nfils = int(data[fil_idx+1])
+        self.filament_dict['nfils'] = nfils
+        print('nfils,', nfils)
+
+        #store all data for filaments in here
+        self.filament_dict['filaments'] = []
+
+        fil_add = fil_idx+2
+        for i in range(nfils):
+            i = 0
+            fil_dict = {}
+            
+            i += fil_add #make sure you are at the right line in the data list 
+            fil_info = data[i]
+            
+            cp1_idx, cp2_idx, nsamp = convert_to_list(fil_info)
+            nsamp = int(nsamp)
+            
+            fil_dict['cp1_idx'] = cp1_idx
+            fil_dict['cp2_idx'] = cp2_idx
+            fil_dict['nsamp'] = nsamp
+            fil_dict['px,py'] = []
+
+            
+            for k in range(nsamp):
+
+                i += 1
+                positions = data[i]
+                px,py= convert_to_list(positions)
+                #print('px,py,pz:',px,py,pz)
+                fil_dict['px,py'].append([px,py])
+            
+            #make this to fill out later
+            fil_dict['Field Vals'] = []
+
+            #add filament info to dict
+            self.filament_dict['filaments'].append(fil_dict)
+            fil_add = i + 1
+
+        cp_dat_idx = i + 1
+
+
+        #Field Data
+        print('Reading data fields:')
+        nb_cp_dat_fields = int(data[cp_dat_idx+1])
+        cp_dat_add = cp_dat_idx+2
+        self.filament_dict['nb_CP_fields'] = nb_cp_dat_fields
+        self.filament_dict['CP_fields'] = []
+
+        for i in range(nb_cp_dat_fields):
+            i = 0
+            i += cp_dat_add #make sure you are at the right line in the data list 
+            cp_field_info = data[i]
+            print('CP field:',cp_field_info)
+            self.filament_dict['CP_fields'].append(cp_field_info)
+            
+            cp_dat_add = i + 1
+
+        cp_field_val_idx = i + 1 
+
+        cp_val_add = cp_field_val_idx
+        cp_field_vals = []
+        for i in range(ncrit):
+            i = 0
+            i += cp_val_add #make sure you are at the right line in the data list 
+            cp_field_val_info = data[i]
+            list_of_cp_vals = convert_to_list(cp_field_val_info)
+            cp_field_vals.append(list_of_cp_vals)
+            
+            cp_val_add = i + 1
+            
+        fil_dat_idx = i + 1  
+
+        #put the field vals in the right place in the dictionary
+        for j in range(ncrit):
+            self.filament_dict['critical_points'][j]['Field Vals'] = cp_field_vals[j]
+        
+        nb_fil_dat_fields = int(data[fil_dat_idx+1])
+        self.filament_dict['nb_fil_fields'] = nb_fil_dat_fields
+        self.filament_dict['fil_fields'] = []
+        fil_dat_add = fil_dat_idx+2
+        for i in range(nb_fil_dat_fields):
+            i = 0
+            i += fil_dat_add #make sure you are at the right line in the data list 
+            fil_field_info = data[i]
+            print('Filament field:',fil_field_info)
+            self.filament_dict['fil_fields'].append(fil_field_info)
+            
+            fil_dat_add = i + 1
+
+        fil_field_val_idx = i + 1  
+
+        fil_val_add = fil_field_val_idx
+
+        fil_field_vals = []
+        for i in range(nfils):
+            i = 0
+            i += fil_val_add #make sure you are at the right line in the data list 
+            fil_field_val_info = data[i]
+            list_of_fil_vals = convert_to_list(fil_field_val_info)
+            fil_field_vals.append(list_of_fil_vals)
+            
+            fil_val_add = i + 1
+        #put the field vals in the right place in the dictionary
+        for j in range(nfils):
+            self.filament_dict['filaments'][j]['Field Vals'] = fil_field_vals[j]
+    
+
+    
+        t1 = time.time()
+        print('Reading filaments took {:0.2f} secs.'.format(t1 - t0))
+            
+        return self.filament_dict
+        
+        
+   
+    
+class ReadFilament:
+    def __init__(self,file_path=None):
+        
+        """
+        Make a filaments dictionary out of ASCII NDSKL file
+        """
+        self.file_path = file_path
+        self.filament_dict = None
+        self.read_data()
+   
+
+        
+    def read_data(self):
+        t0 = time.time()
+        self.filament_dict = {}
+
+        #read the file first and write each line into data
+        data = []
+        f = open(self.file_path,'r')
+        for line in f:
+            data.append(line)
+        f.close()
+        
+        def convert_to_list(ascii_chars,type=float):
+            #strip whitespace from ends
+            ascii_chars = str(ascii_chars)
+
+            char_list = list(ascii_chars.split(" "))
+            char_list = ' '.join(char_list).split()
+            
+
+            p_list = list(map(type, char_list))
+            
+            return p_list
+        
+        header1 = data[0]
+        print('header1,',header1)
+        
+        ndims = data[1]
+        print('ndims,', ndims)
+
+        comments = data[2]
+        print('Comments,',comments)
+
+        extent = data[3]
+        print('Bounding box,', extent)
+
+        #data[4] is str(Critical Points)
+
+        ncrit = int(data[5])
+        print('ncrit,', ncrit)
+        self.filament_dict['ncrit'] = ncrit
+
+        #store all data for critical points in here
+        self.filament_dict['critical_points'] = []
+
+        ##### CPs
+        
+        add_to_idx = 6 
+        for i in range(ncrit):
+            cp_dict = {}
+            i = 0
+            
+            i += add_to_idx #make sure you are at the right line in the data list 
+            critical_vals = data[i]
+            
+            c_idx, px, py, pz, value, pairID, boundary = convert_to_list(critical_vals)
+            #next line in data
+
+            cp_dict['cp_idx']  = c_idx
+            cp_dict['px'] = px 
+            cp_dict['py'] = py
+            cp_dict['pz'] = pz
+            cp_dict['pair_ID'] = pairID
+            cp_dict['boundary'] = boundary 
+
+
+            i += 1
+            nfil = int(data[i])
+            cp_dict['nfil'] = nfil
+            cp_dict['destID,filID'] = []
+            
+            for k in range(nfil):
+
+                i += 1
+                cp_on_fil = data[i]
+                destID, filID = convert_to_list(cp_on_fil,int)
+        
+                cp_dict['destID,filID'].append([destID,filID])
+
+            #make this to fill out later
+            cp_dict['Field Vals'] = []
+            #add all info to cp dict
+            self.filament_dict['critical_points'].append(cp_dict)
+            
+            add_to_idx = i + 1
+
+
+        ##### Filaments
+
+        fil_idx = i + 1
+        nfils = int(data[fil_idx+1])
+        self.filament_dict['nfils'] = nfils
+        print('nfils,', nfils)
+
+        #store all data for filaments in here
+        self.filament_dict['filaments'] = []
+
+        fil_add = fil_idx+2
+        for i in range(nfils):
+            i = 0
+            fil_dict = {}
+            
+            i += fil_add #make sure you are at the right line in the data list 
+            fil_info = data[i]
+            
+            cp1_idx, cp2_idx, nsamp = convert_to_list(fil_info)
+            nsamp = int(nsamp)
+            
+            fil_dict['cp1_idx'] = cp1_idx
+            fil_dict['cp2_idx'] = cp2_idx
+            fil_dict['nsamp'] = nsamp
+            fil_dict['px,py,pz'] = []
+
+            
+            for k in range(nsamp):
+
+                i += 1
+                positions = data[i]
+                px,py,pz = convert_to_list(positions)
+                #print('px,py,pz:',px,py,pz)
+                fil_dict['px,py,pz'].append([px,py,pz])
+            
+            #make this to fill out later
+            fil_dict['Field Vals'] = []
+
+            #add filament info to dict
+            self.filament_dict['filaments'].append(fil_dict)
+            fil_add = i + 1
+
+        cp_dat_idx = i + 1
+
+
+        #Field Data
+        print('Reading data fields:')
+        nb_cp_dat_fields = int(data[cp_dat_idx+1])
+        cp_dat_add = cp_dat_idx+2
+        self.filament_dict['nb_CP_fields'] = nb_cp_dat_fields
+        self.filament_dict['CP_fields'] = []
+
+        for i in range(nb_cp_dat_fields):
+            i = 0
+            i += cp_dat_add #make sure you are at the right line in the data list 
+            cp_field_info = data[i]
+            print('CP field:',cp_field_info)
+            self.filament_dict['CP_fields'].append(cp_field_info)
+            
+            cp_dat_add = i + 1
+
+        cp_field_val_idx = i + 1 
+
+        cp_val_add = cp_field_val_idx
+        cp_field_vals = []
+        for i in range(ncrit):
+            i = 0
+            i += cp_val_add #make sure you are at the right line in the data list 
+            cp_field_val_info = data[i]
+            list_of_cp_vals = convert_to_list(cp_field_val_info)
+            cp_field_vals.append(list_of_cp_vals)
+            
+            cp_val_add = i + 1
+            
+        fil_dat_idx = i + 1  
+
+        #put the field vals in the right place in the dictionary
+        for j in range(ncrit):
+            self.filament_dict['critical_points'][j]['Field Vals'] = cp_field_vals[j]
+        
+        nb_fil_dat_fields = int(data[fil_dat_idx+1])
+        self.filament_dict['nb_fil_fields'] = nb_fil_dat_fields
+        self.filament_dict['fil_fields'] = []
+        fil_dat_add = fil_dat_idx+2
+        for i in range(nb_fil_dat_fields):
+            i = 0
+            i += fil_dat_add #make sure you are at the right line in the data list 
+            fil_field_info = data[i]
+            print('Filament field:',fil_field_info)
+            self.filament_dict['fil_fields'].append(fil_field_info)
+            
+            fil_dat_add = i + 1
+
+        fil_field_val_idx = i + 1  
+
+        fil_val_add = fil_field_val_idx
+
+        fil_field_vals = []
+        for i in range(nfils):
+            i = 0
+            i += fil_val_add #make sure you are at the right line in the data list 
+            fil_field_val_info = data[i]
+            list_of_fil_vals = convert_to_list(fil_field_val_info)
+            fil_field_vals.append(list_of_fil_vals)
+            
+            fil_val_add = i + 1
+        #put the field vals in the right place in the dictionary
+        for j in range(nfils):
+            self.filament_dict['filaments'][j]['Field Vals'] = fil_field_vals[j]
+    
+
+    
+        t1 = time.time()
+        print('Reading filaments took {:0.2f} secs.'.format(t1 - t0))
+            
+        return self.filament_dict
