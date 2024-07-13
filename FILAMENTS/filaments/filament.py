67122315bcec31b55fdfe31a1d8aa5ed2faac648
Daniel Gallego <dangallego3@gmail.com>
2024-07-13 13:08:51 -0400

    adds method to retrieve Filament lengths to Filament Class. Also renames other methods in the class to standardized naming scheme.

--- FILAMENTS/filaments/filament.py
+++ FILAMENTS/filaments/filament.py
@@ -13,10 +13,10 @@ import read_fils as rf
 class Filament:
     def __init__(self, path_to_filament_NDskl):
         self.path_to_filament_NDskl = path_to_filament_NDskl
-        self.filament_dict = self.import_fil()
-        #self.print_filament_dict
+        self.filament_dict = self.import_fils()
 
-    def import_fil(self):
+
+    def import_fils(self):
         '''
         Imports filaments and creates dictionary containing filament coordinates and metrics
         based on a given path to filament skeleton file (.NDskl file).
@@ -36,13 +36,16 @@ class Filament:
         # sys.path.remove(self.path_to_filament_NDskl)
 
         return filament_dm_dict
-    
-
-    def print_filament_dict(self):
-        print(self.filament_dict)
 
 
-    def DataFrame(self):
+    def filament_coordinates(self):
+        '''
+        Creates a Pandas DataFrame of filament coordinates, with ID's corresponding to each half segment 
+        (Node -> Saddle Point).
+
+        Returns:
+        df (Pandas.DataFrame): DataFrame of filament px py pz coordinates and Filament ID. 
+        '''
         df = pd.DataFrame()
         for i in range(self.filament_dict['nfils']):
             p = self.filament_dict['filaments'][i]['px,py,pz']
@@ -52,58 +55,115 @@ class Filament:
         return df
     
     
-    def filament_segments(self):
-        df = pd.DataFrame()
+    def segment_coordinates(self):
+        '''
+        Creates a Pandas DataFrame of filament segments and their coordinates and Filament ID's. 
+        Each segment consists of two points, corresponding to (px,py,pz) and (px2,py2,pz2). 
+        Compared to the Filament_Coordinates method, this will result in one less row per Filament ID (due to the "picket fence" problem).
+
+        Returns: 
+        fil_segments (Pandas.DataFrame): DataFrame of filament segments for each Filament ID. 
+        '''
+        fil_segments = pd.DataFrame()
         for i in range(self.filament_dict['nfils']):
             p = np.array(self.filament_dict['filaments'][i]['px,py,pz'])
             p = np.hstack([p[:-1],p[1:]])
             p = pd.DataFrame(p,columns = ['px', 'py', 'pz','px2','py2','pz2'])
             p['Filament ID'] = i
-            df = pd.concat([df, p])
-        return df
+            fil_segments = pd.concat([fil_segments, p])
+        return fil_segments
     
 
-    def slice(self, value):
+    def get_cp(self, cp_id):
+        '''
+        Slices filament_dm_dict based on a specified critical point ID.
+        CRITICAL POINT ID's:
+        1: Voids
+        2: Walls
+        3: Saddle Points
+        4: Nodes
+        5: Bifurcation Points
+
+        Returns: 
+        cp_list (dict): Dictionary of a specified critical point type.
+        '''
         critical_points = self.filament_dict['critical_points']
         N = len(critical_points)
         cp_list = [] 
         for i in range(N):
             C = critical_points[i]['cp_idx']
-            if C == value:
+            if C == cp_id:
                 cp_list.append(critical_points[i])
         return cp_list 
     
     
     def voids(self):
-        return self.dict_slice(0)
+        return self.get_cp(0)
     
     def walls(self):
-        return self.dict_slice(1)
+        return self.get_cp(1)
     
     def saddles(self):
-        return self.dict_slice(2)
+        return self.get_cp(2)
     
     def nodes(self):
-        return self.dict_slice(3)
+        return self.get_cp(3)
     
     def bifurcation_points(self):
-        return self.dict_slice(4)
+        return self.get_cp(4)
+    
+    
+    def lengths(self, DataFrame=True):
+        '''
+        Calculates the lengths of each (half) filament based on the sub-segments of each unique Filament ID. 
+        Gives option of returning DataFrame with filament segments, IDs and lengths or just the lengths corresponding to each unique Filament ID.
 
+        Parameters:
+        DataFrame (bool): Determines whether DataFrame of filament segments, IDs and lengths will be returned. Set to False to return array instead.
 
+        Returns:
+        segments (Pandas.DataFrame): DataFrame of Filament segments, IDs and lengths. 
+            or
+        filament_lengths (array-like): Array of lengths correspinding to each unique Filament ID. 
+        '''
+        segments = self.segment_coordinates()
+        # For ease/speed we convert to Numpy arrays 
+        segment_array = np.array(segments)
 
-#function to slice a dictionary list by a particular set of keys 
-def dict_slice(dict, key, value):
-    '''Indexes/slices a subset of a larger dictionary. 
-        For use with separating critical points (since the CP id's are given).
-        Output is a list of dictionaries.'''
-    N = len(dict)
-    list = []
-    for i in range(N):
-        C = dict[i][key]
-        if C == value:
-            list.append(dict[i])
-    return list 
+        # Calculate the distance between segments for each Filament ID; note that no ID is stored 
+        L = len(segment_array)
+        segment_distances = np.zeros(L)
+        for i in range(L):
+            segment_distances[i] = np.linalg.norm((segment_array[i][:3]) - (segment_array[i][3:-1]))
+            
+        # Check how many unique Filament ID's there are
+        filament_ids = np.array(segments["Filament ID"])
+        unique_ids = np.unique(filament_ids)
+                        
+        # Next loop over the unique Filament IDs and sum distances between segments for each Filament ID
+        M = len(unique_ids) # length of unique ids, from 0 to 7340
+        filament_lengths = np.zeros(M)
+        for i in range(M): 
+            ID_mask = np.where(filament_ids == i)  # masks entire filament length array by filament ID (which should == iterator step)
+            filament_lengths[i] = np.sum(segment_distances[ID_mask]) # sums all of the non-masked values from iterator check, saves to i-th value of new array
 
+        if DataFrame == True:
+            # Save Filament Lengths for each Filament ID (necessary step given that DataFrame has multiple rows of the same ID's)
+            N = len(filament_ids) # larger array 
+            P = len(filament_lengths) # smaller array 
+            all_filament_lengths = np.zeros(N)
+            for i in range(P): 
+                ID_mask = np.where(filament_ids == i)
+                all_filament_lengths[ID_mask] = filament_lengths[i]
+            
+            # Add columnn to DataFrame
+            segments['Filament Length'] = all_filament_lengths
+
+            return segments
+        
+        else:
+            # Instead of entire DataFrame, return only the Filament Lengths corresponding to each unique Filament ID
+            return filament_lengths
 
 
         
